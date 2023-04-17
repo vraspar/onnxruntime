ae104d4fd3ccf4a762db2625e3def334ab343c2b
Guangyun Han <guangyunhan@microsoft.com>
2023-04-17 10:47:42 +0000

    Add C# binding for rocm ep and add missing api from C++ side

--- csharp/src/Microsoft.ML.OnnxRuntime/ProviderOptions.shared.cs
+++ csharp/src/Microsoft.ML.OnnxRuntime/ProviderOptions.shared.cs
@@ -207,6 +207,98 @@ namespace Microsoft.ML.OnnxRuntime
 
 
     /// <summary>
+    /// Holds the options for configuring a ROCm Execution Provider instance
+    /// </summary>
+    public class OrtROCMProviderOptions : SafeHandle
+    {
+        internal IntPtr Handle
+        {
+            get
+            {
+                return handle;
+            }
+        }
+
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructs an empty OrtROCMroviderOptions instance
+        /// </summary>
+        public OrtROCMProviderOptions() : base(IntPtr.Zero, true)
+        {
+            NativeApiStatus.VerifySuccess(NativeMethods.OrtCreateROCMProviderOptions(out handle));
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Get ROCm EP provider options
+        /// </summary>
+        /// <returns> return C# UTF-16 encoded string </returns>
+        public string GetOptions()
+        {
+            var allocator = OrtAllocator.DefaultInstance;
+
+            // Process provider options string
+            IntPtr providerOptions = IntPtr.Zero;
+            NativeApiStatus.VerifySuccess(NativeMethods.OrtGetROCMProviderOptionsAsString(handle, allocator.Pointer, out providerOptions));
+            using (var ortAllocation = new OrtMemoryAllocation(allocator, providerOptions, 0))
+            {
+                return NativeOnnxValueHelper.StringFromNativeUtf8(providerOptions);
+            }
+        }
+
+        /// <summary>
+        /// Updates  the configuration knobs of OrtROCMProviderOptions that will eventually be used to configure a ROCm EP
+        /// Please refer to the following on different key/value pairs to configure a ROCm EP and their meaning:
+        /// https://onnxruntime.ai/docs/execution-providers/ROCm-ExecutionProvider.html
+        /// </summary>
+        /// <param name="providerOptions">key/value pairs used to configure a ROCm Execution Provider</param>
+        public void UpdateOptions(Dictionary<string, string> providerOptions)
+        {
+
+            using (var cleanupList = new DisposableList<IDisposable>())
+            {
+                var keysArray = NativeOnnxValueHelper.ConvertNamesToUtf8(providerOptions.Keys.ToArray(), n => n, cleanupList);
+                var valuesArray = NativeOnnxValueHelper.ConvertNamesToUtf8(providerOptions.Values.ToArray(), n => n, cleanupList);
+
+                NativeApiStatus.VerifySuccess(NativeMethods.OrtUpdateROCMProviderOptions(handle, keysArray, valuesArray, (UIntPtr)providerOptions.Count));
+            }
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Overrides SafeHandle.IsInvalid
+        /// </summary>
+        /// <value>returns true if handle is equal to Zero</value>
+        public override bool IsInvalid { get { return handle == IntPtr.Zero; } }
+
+        #endregion
+
+        #region SafeHandle
+        /// <summary>
+        /// Overrides SafeHandle.ReleaseHandle() to properly dispose of
+        /// the native instance of OrtROCMProviderOptions
+        /// </summary>
+        /// <returns>always returns true</returns>
+        protected override bool ReleaseHandle()
+        {
+            NativeMethods.OrtReleaseROCMProviderOptions(handle);
+            handle = IntPtr.Zero;
+            return true;
+        }
+
+        #endregion
+    }
+
+
+    /// <summary>
     /// This helper class contains methods to handle values of provider options
     /// </summary>
     public class ProviderOptionsValueHelper
--- onnxruntime/core/session/onnxruntime_c_api.cc
+++ onnxruntime/core/session/onnxruntime_c_api.cc
@@ -2725,7 +2725,12 @@ static constexpr OrtApi ort_api_1_to_15 = {
     &OrtApis::CastTypeInfoToOptionalTypeInfo,
     &OrtApis::GetOptionalContainedTypeInfo,
     &OrtApis::GetResizedStringTensorElementBuffer,
-    &OrtApis::KernelContext_GetAllocator};
+    &OrtApis::KernelContext_GetAllocator,
+    &OrtApis::CreateROCMProviderOptions,
+    &OrtApis::UpdateROCMProviderOptions,
+    &OrtApis::GetROCMProviderOptionsAsString,
+    &OrtApis::ReleaseROCMProviderOptions
+};
 
 // Asserts to do a some checks to ensure older Versions of the OrtApi never change (will detect an addition or deletion but not if they cancel out each other)
 // If any of these asserts hit, read the above 'Rules on how to add a new Ort API version'
